fed37477b892398c3e780a865ebeed526df4c786
Shubham Goyal <shubham807.sg@gmail.com>
2024-01-11 03:51:57 +0530

    validation on signup and login

--- controllers/user.js
+++ controllers/user.js
@@ -3,45 +3,72 @@ const User = require("../models/user");
 const { setUser } = require("../service/auth");
 
 // callback function for signup api
-async function handleUserSignup(req, res) {
-  console.log(req.body)
-  // name, email and password from request body
-  const { name, email, password } = req.body;
+async function handleUserSignup(req, res){
+  let error = ''
+  if(!req.body.name){
+    error = error + 'Name, '
+  }
+  if(!req.body.email){
+    error = error + 'Email, '
+  }
+  if(!req.body.password){
+    error = error + 'Password, '
+  }
+  if(error){
+    error = error.slice(0, -2)  + ' is required'
+    return res.render("signup", { invalid : error});
+  }
+  else{
+    // name, email and password from request body
+    const { name, email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if(user) 
-    return res.render("signup", { message : 'Email already exist'});
-  // adding the  user in the user database
-  await User.create({
-    name,
-    email,
-    password,
-  });
-  return res.render("login", { message : 'Successfully Signed Up, Login to continue'});
+    if(user) 
+      return res.render("signup", { message : 'Email already exist'});
+    // adding the  user in the user database
+    await User.create({
+      name,
+      email,
+      password,
+    });
+    return res.render("login", { message : 'Successfully Signed Up, Login to continue'});
+  }
 }
 
 // callback function for login api
 async function handleUserLogin(req, res) {
+  let error = ''
+  if(!req.body.email){
+    error = error + 'Email, '
+  }
+  if(!req.body.password){
+    error = error + 'Password, '
+  }
+  if(error){
+    error = error.slice(0, -2)  + ' is required'
+    return res.render("login", { invalid : error});
+  }
+  else{
+      // getting email and password from request body
+      const { email, password } = req.body;
 
-  // getting email and password from request body
-  const { email, password } = req.body;
+      // finding the user from db
+      const user = await User.findOne({ email, password });
 
-  // finding the user from db
-  const user = await User.findOne({ email, password });
+      if (!user)
+        return res.render("login", { invalid : "Invalid Username or Password"});
 
-  if (!user)
-    return res.render("login", { invalid : "Invalid Username or Password"});
+      // generating a session id for this login
+      const sessionId = uuidv4();
 
-  // generating a session id for this login
-  const sessionId = uuidv4();
+      // maping this user to this session id in sessionIdToUserMap 
+      setUser(sessionId, user);
 
-  // maping this user to this session id in sessionIdToUserMap 
-  setUser(sessionId, user);
-
-  // setting the cookie
-  res.cookie("uid", sessionId);
-  return res.redirect("/");
+      // setting the cookie
+      res.cookie("uid", sessionId);
+      return res.redirect("/");
+  }
 }
 
 async function handleUserLogout(req, res) {
